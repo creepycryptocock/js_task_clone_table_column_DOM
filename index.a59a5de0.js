220527cc0b4e3c159380a048a5ef521d20ac6218
Yurii <j.zherebilov15@gmail.com>
2025-06-13 01:31:23 +0300

    make build

---
+++ index.a59a5de0.js
@@ -0,0 +1,2 @@
+!function(){for(var e=document.querySelector(".table").rows,l=0;l<e.length;l++){var o=e[l],r=o.cells[1].cloneNode(!0);o.insertBefore(r,o.cells[o.cells.length-1])}}();
+//# sourceMappingURL=index.a59a5de0.js.map
